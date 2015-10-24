5d8e8fd01df7467c5e3ef12df2876b9bbadab0dd
RobertDober <robert.dober@gmail.com>
2015-10-24 21:29:32 +0200

    fixed syn err in config/prod.exs:18

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :earmark_dingus, EarmarkDingus.Endpoint,
   http: [port: {:system, "PORT"}],
   # url: [host: "example.com", port: 80],
-  url: [host: "sleepy-depths-5324.herokuapp.com", port: 80]
+  url: [host: "sleepy-depths-5324.herokuapp.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
