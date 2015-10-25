4e8f6085a5e16455d4e4eb93cba0f7e36220b0ce
RobertDober <robert.dober@gmail.com>
2015-10-25 08:49:56 +0100

    assign @version in controller

--- web/controllers/earmark_controller.ex
+++ web/controllers/earmark_controller.ex
@@ -1,17 +1,20 @@
-
 defmodule EarmarkDingus.EarmarkController do
   use EarmarkDingus.Web, :controller
+
 
   @allowed_input_length 1000
 
   def show conn, %{"text" => text} do 
-    # if String.length(text) <= @allowed_input_length  do
-      render conn, "show.json", html: Earmark.to_html( text )
-    # else
-    #   conn
-    #   |> put_status( 412 )
-    #   |> json( %{error_message: "text parameter exceeds authorized length of #{@allowed_input_length}"} )
-    # end
+    if String.length(text) <= @allowed_input_length  do
+      render conn,
+        "show.json",
+         html: Earmark.to_html( text ), 
+         version: Earmark.Mixfile.project[:version]
+    else
+      conn
+      |> put_status( 412 )
+      |> json( %{error_message: "text parameter exceeds authorized length of #{@allowed_input_length}"} )
+    end
   end
 
   def show conn, _params do
