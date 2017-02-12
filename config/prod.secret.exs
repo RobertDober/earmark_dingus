eb74d3ed89debfd4d376a653402ffc0d005edeea
RobertDober <robert.dober@gmail.com>
2017-02-12 10:25:50 +0100

    some config fixes

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -3,11 +3,11 @@ use Mix.Config
 # In this file, we keep production configuration that
 # # you likely want to automate and keep it away from
 # # your version control system.
-# config :heroku_test_app, HerokuTestApp.Endpoint,
-secret_key_base: System.get_env("SECRET_KEY_BASE")
+config :earmark_dingus2, EarmarkDingus2.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 #
 #   # Configure your database
-config :heroku_test_app, HerokuTestApp.Repo,
+config :earmark_dingus2, EarmarkDingus2.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  size: 20 # The amount of database connections in the pool
+  size: 1 # The amount of database connections in the pool
