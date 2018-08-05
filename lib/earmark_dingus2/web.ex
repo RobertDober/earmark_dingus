fc0e5b033d95173e04fcca2eb429d7621e028922
RobertDober <robert.dober@gmail.com>
2018-08-05 11:10:45 +0200

    removing dead code

--- lib/earmark_dingus2/web.ex
+++
@@ -1,7 +0,0 @@
-defmodule EarmarkDingus2.Web do
-
-  def start_link do
-    port = System.get_env("PORT") |> String.to_integer
-    Plug.Adapters.Cowboy.http(EarmarkDingus2.Api, [], port: port)
-  end
-end
